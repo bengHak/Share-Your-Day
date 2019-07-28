8bd2872dc20a6ea31ae32d4ef6cf57ab4f5c551a
PanGeun <lifebook0809@gmail.com>
2019-07-28 16:53:08 +0900

    add login & logout func. in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def signup(request):
@@ -6,7 +6,23 @@ def signup(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, login)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
 
 
 def index(request):
