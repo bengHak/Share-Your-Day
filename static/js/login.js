769b37404bf08a5ed39c12def8e7753e824b4feb
lab <qkwl4678@naver.com>
2019-08-09 16:44:06 +0900

    before deploy

---
+++ static/js/login.js
@@ -0,0 +1,17 @@
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 339,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+    });
+}
